97e7a25164b02a9e58d367f84986808856bcbf89
Dani White <dmwhite@mit.edu>
2020-04-22 15:46:09 -0700

    Version I plan on submitting

--- dynamics_test.py
+++ dynamics_test.py
@@ -1,6 +1,6 @@
 """"Set up dynamics for cornering and visualize them."""
 
-import math
+import numpy as np
 import matplotlib.pyplot as plt
 import pydrake.symbolic as sym
 
@@ -8,13 +8,15 @@ from pydrake.systems.analysis import Simulator
 from pydrake.systems.framework import DiagramBuilder
 from pydrake.systems.primitives import LogOutput, SymbolicVectorSystem
 
-# Set up car parameters (copied from a car parameter file in the sim repo)
+# Set up car parameters
 m = 276  # kg
 Iz = 180.49  # kg*m^2
-l_F = 0.583*60*0.0254  # m
-l_R = (1-0.583)*60*0.0254  # m
+# Changed to be neutral steer
+RWB = 0.5  # 0.583
+l_F = RWB*60*0.0254  # m
+l_R = (1-RWB)*60*0.0254  # m
 
-# Tire parameters
+# Hand-wavy tire parameters
 # Longitudinal stiffness guessed using tire forces ~ 1000 N, slip ratio ~ 0.1 -> 1000/0.1=1e4
 S_FL = 1e4
 S_RL = 1e4
@@ -31,44 +33,61 @@ delta = sym.Variable("u3")
 v_x = sym.Variable("x1")
 v_y = sym.Variable("x2")
 r = sym.Variable("x3")
+x = sym.Variable("x4")
+y = sym.Variable("x5")
+psi = sym.Variable("x6")
 
-# Alphas
+# Slip angles
 alpha_F = sym.atan2(v_y+l_F*r, v_x) - delta
 alpha_R = sym.atan2(v_y+l_F*r, v_x)
 
+dynamics = [
+    (sym.cos(delta)*S_FL*kappa_F - sym.sin(delta)
+     * S_FC*alpha_F + S_RL*kappa_R)/m - v_y*r,
+    (sym.sin(delta)*S_FL*kappa_F + sym.cos(delta)
+     * S_FC*alpha_F + S_RC*alpha_R)/m + v_x*r,
+    l_F*(sym.sin(delta)*S_FL*kappa_F + sym.cos(delta)
+         * S_FC*alpha_F) - l_R*S_RC*alpha_R,
+    v_y*sym.cos(psi) - v_x*sym.sin(psi),
+    v_y*sym.sin(psi) + v_x*sym.cos(psi),
+    r
+]
+
 vector_system = SymbolicVectorSystem(
-    state=[v_x, v_y, r],
+    state=[v_x, v_y, r, x, y, psi],
     input=[kappa_F, kappa_R, delta],
-    dynamics=[
-        (sym.cos(delta)*S_FL*kappa_F - sym.sin(delta)*S_FC*alpha_F + S_RL*kappa_R)/m,
-        (sym.sin(delta)*S_FL*kappa_F + sym.cos(delta)*S_FC*alpha_F + S_RC*alpha_R)/m,
-        l_F*(sym.sin(delta)*S_FL*kappa_F + sym.cos(delta)
-             * S_FC*alpha_F) - l_R*S_RC*alpha_R
-    ],
-    output=[v_x, v_y, r])
+    dynamics=dynamics,
+    output=[v_x, v_y, r, x, y, psi])
+
 builder = DiagramBuilder()
 system = builder.AddSystem(vector_system)
 logger = LogOutput(system.get_output_port(0), builder)
 builder.ExportInput(system.get_input_port(0))
-
 diagram = builder.Build()
 
-# Set the initial conditions, x(0).
+# Initial conditions
+x0 = [1, 1, 0, 0, 0, 0]
 context = diagram.CreateDefaultContext()
-context.SetContinuousState([0, 0, 0])
+context.SetContinuousState(x0)
 
+# Fix input
+u = [1, 1, 0]
 inp = system.get_input_port(0)
-inp.FixValue(context, [0, 0, 0])
+inp.FixValue(context, u)
 
 # Create the simulator, and simulate for 10 seconds.
 simulator = Simulator(diagram, context)
 simulator.Initialize()
 simulator.AdvanceTo(10)
 
-# Plot the results.
+# Plots
 v_x_data = logger.data()[0, :]
 v_y_data = logger.data()[1, :]
 r_data = logger.data()[2, :]
+x_data = logger.data()[3, :]
+y_data = logger.data()[4, :]
+
+vel_mag = (v_x_data**2 + v_y_data**2)**0.5
 
 plt.subplot(311)
 plt.plot(logger.sample_times(), v_x_data)
@@ -84,5 +103,19 @@ plt.subplot(313)
 plt.plot(logger.sample_times(), r_data)
 plt.xlabel('$t$')
 plt.ylabel('$r(t)$')
+
+plt.figure()
+plt.title("Lateral tire forces")
+plt.plot(logger.sample_times(), np.arctan2(
+    v_y_data+l_F*r_data, v_x_data) - u[2], label="$F_\{Y\}$")
+plt.xlabel("$t$")
+plt.ylabel("$F_Y$")
+
+plt.figure()
+plt.scatter(x_data, y_data, c=vel_mag)
+plt.xlabel("$x$")
+plt.ylabel("$y$")
+cb = plt.colorbar()
+cb.set_label("Speed")
 
 plt.show()
