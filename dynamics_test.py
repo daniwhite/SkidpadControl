4caf80e279c6f7000953d24c2cfd1488c5bed5bf
Dani White <dmwhite@mit.edu>
2020-04-22 09:49:06 -0700

    Configure dynamics with fixed input

--- dynamics_test.py
+++ dynamics_test.py
@@ -22,17 +22,43 @@ S_RL = 1e4
 S_FC = 1e3
 S_RC = 1e3
 
-x = sym.Variable("x")
-vector_system = SymbolicVectorSystem(state=[x], dynamics=[0.1], output=[x])
+# Inputs
+kappa_F = sym.Variable("u1")
+kappa_R = sym.Variable("u2")
+delta = sym.Variable("u3")
 
+# Outputs
+v_x = sym.Variable("x1")
+v_y = sym.Variable("x2")
+r = sym.Variable("x3")
+
+# Alphas
+alpha_F = sym.atan2(v_y+l_F*r, v_x) - delta
+alpha_R = sym.atan2(v_y+l_F*r, v_x)
+
+vector_system = SymbolicVectorSystem(
+    state=[v_x, v_y, r],
+    input=[kappa_F, kappa_R, delta],
+    dynamics=[
+        (sym.cos(delta)*S_FL*kappa_F - sym.sin(delta)*S_FC*alpha_F + S_RL*kappa_R)/m,
+        (sym.sin(delta)*S_FL*kappa_F + sym.cos(delta)*S_FC*alpha_F + S_RC*alpha_R)/m,
+        l_F*(sym.sin(delta)*S_FL*kappa_F + sym.cos(delta)
+             * S_FC*alpha_F) - l_R*S_RC*alpha_R
+    ],
+    output=[v_x, v_y, r])
 builder = DiagramBuilder()
 system = builder.AddSystem(vector_system)
 logger = LogOutput(system.get_output_port(0), builder)
+builder.ExportInput(system.get_input_port(0))
+
 diagram = builder.Build()
 
 # Set the initial conditions, x(0).
 context = diagram.CreateDefaultContext()
-context.SetContinuousState([0.9])
+context.SetContinuousState([0, 0, 0])
+
+inp = system.get_input_port(0)
+inp.FixValue(context, [0, 0, 0])
 
 # Create the simulator, and simulate for 10 seconds.
 simulator = Simulator(diagram, context)
